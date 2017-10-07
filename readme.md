6b6ece5bd41fecb94589b8a3eda43c3687a80e25
Matthew Rikard <mrikard@geninfo.com>
2017-10-07 17:36:51 -0400

    copies and pastes, yep

---
+++ readme.md
@@ -0,0 +1,4 @@
+# WinCopyPaste
+
+See https://github.com/sindresorhus/win-clipboard, solves \r\r\n issue as well as compiled for both x86 and x64
+
---
+++ wincopy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace wincopy
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			var contentb = new StringBuilder();
+			string line;
+			while ((line = Console.In.ReadLine()) != null)
+			{
+				contentb.AppendLine(line);
+			}
+			string content = contentb.ToString();
+
+			Clipboard.SetText(content);
+		}
+	}
+}
